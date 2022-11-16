e2cb2197bf4318bbbdfa0b3ff1f1e8e1dc6f30b4
kcashique <ashiquekc97@gmail.com>
2022-11-16 12:24:15 +0530

    up

--- web/context_proccessor.py
+++ web/context_proccessor.py
@@ -6,12 +6,13 @@ from services.models import BrandingImage
 
 
 
+
+
 def main_context(request):
-    # user = request.user.username
-    # print(user)
-    # if request.user.is_authenticated:
-    #     if user.session_set.all().count() > settings.MAX_DEVICE_SESSIONS:
-    #         user.session_set.all().order_by('-last_activity')[:3].delete()
+
+    if request.user.is_authenticated:
+        if request.user.session_set.all().count() > settings.MAX_DEVICE_SESSIONS:
+            request.user.session_set.all().order_by('-last_activity')[:settings.MAX_DEVICE_SESSIONS].delete()
 
     branding_image = BrandingImage.objects.all()
 
