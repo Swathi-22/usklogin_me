5668aff2116023dbdaa8e47103e164c746099a90
kcashique <ashiquekc97@gmail.com>
2022-12-07 12:11:42 +0530

    decerator issue fixed

--- web/decorators.py
+++ web/decorators.py
@@ -3,14 +3,17 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Order
+from . models import Order
+
 
 
 def subscription_required(func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
-            paln = Order.objects.filter(user=request.user, is_active=True).last()
-            if plan.is_active:
+            subs = Order.objects.filter(user=request.user, is_active=True).last()
+            if request.user.is_superuser:
+                return func(request, *args, **kwargs)
+            elif subs.is_valid:
                 return func(request, *args, **kwargs)
             else:
                 return redirect("web:expired")
