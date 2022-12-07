bb27361f1d3d08b027279c73ab1a1b3e9edf30b5
Swathi-22 <mkswathisuresh@gmail.com>
2022-12-07 12:16:18 +0530

    7/12_Updation

--- web/decorators.py
+++ web/decorators.py
@@ -9,11 +9,12 @@ from .models import Order
 def subscription_required(func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
-            paln = Order.objects.filter(user=request.user, is_active=True).last()
-            if plan.is_active:
-                return func(request, *args, **kwargs)
-            else:
-                return redirect("web:expired")
+            # paln = Order.objects.filter(user=request.user, is_active=True).last()
+            # if plan.is_active:
+            #     return func(request, *args, **kwargs)
+            # else:
+            #     return redirect("web:expired")
+            pass
         else:
             return redirect("web:login_view")
     return wrapper
