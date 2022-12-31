540361dab69f12038bf8c89fa3282a94d3b87252
PV Anfas <anfaspv.info@gail.com>
2022-12-31 17:54:48 +0530

    up

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,5 @@
 from .models import FAQ
-from .models import AddonServices
+# from .models import AddonServices
 from .models import AgencyPortal
 from .models import AgentBonus
 from .models import BackOfficeServices
@@ -135,9 +135,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ("user", "amount", "status", "valid_from", "valid_upto")
 
 
-@admin.register(AddonServices)
-class AddonServicesAdmin(admin.ModelAdmin):
-    list_display = ("name", "link")
+# @admin.register(AddonServices)
+# class AddonServicesAdmin(admin.ModelAdmin):
+#     list_display = ("name", "link")
 
 
 @admin.register(OnloadPopup)
