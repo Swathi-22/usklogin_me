d35de8498af39fc9d87d7e7274a9d66d19d4fcb4
PV ANFAS <anfaspv.info@gmail.com>
2023-01-05 08:49:51 +0000

    up

--- web/decorators.py
+++ web/decorators.py
@@ -7,16 +7,10 @@ from django.utils import timezone
 def subscription_required(func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
-            if Subscription.objects.filter(user=request.user, status="Success", types="Access").exists():
-                subscriptions = Subscription.objects.filter(user=request.user, status="Success", types="Access")
-                subs = subscriptions.filter(valid_upto__gt=timezone.now())
-                if request.user.is_superuser or subs:
-                    return func(request, *args, **kwargs)
-                else:
-                    return redirect("web:expired")
+            if Subscription.objects.filter(user=request.user, status="Success", types="Access", valid_upto__gt=timezone.now()).exists():
+                return func(request, *args, **kwargs)
             else:
                 return redirect("web:order_payment")
         else:
             return HttpResponseRedirect("/start/")
-
     return wrapper
